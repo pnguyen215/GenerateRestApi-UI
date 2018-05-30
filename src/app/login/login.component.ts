2be0b359446f3ae0a27de62d86388e740c6cc778
= <=>
2018-05-30 12:30:33 +0700

    v002

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { Login } from '../login';
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+  login : Login  = {
+    user : 'Phuoc Nguyen',
+    password : '*****'
+  }
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
